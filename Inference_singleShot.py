7435a980bc8c89a22d78d39a93a72669acdaf027
Hao Zhao <14055761+haozh7109@users.noreply.github.com>
2024-03-14 10:16:45 +0100

    update

--- Inference_singleShot.py
+++ Inference_singleShot.py
@@ -17,11 +17,7 @@ from Utility.Py_data_Preprocess_Hao import SpectrogramProcess, WaveformDataGener
 from Utility.model import GeneratorSegan, DiscriminatorSegan, VirtualBatchNorm
 from tqdm import tqdm
 
-if __name__ == '__main__':
-
-    # --------------------------------------------------
-    # 1.0 Set training parameters
-    # --------------------------------------------------
+def parse_arguments():
     parser = argparse.ArgumentParser(description='Testing Audio Enhancement with SEGAN')
     parser.add_argument('--batch_size', default=100, type=int, help='train batch size')
     parser.add_argument('--num_epochs', default=100, type=int, help='train epochs number')
@@ -33,54 +29,36 @@ if __name__ == '__main__':
     parser.add_argument('--model_weights_save_path', default='/home/PycharmProjects/pythonProject/Model_Current_Training/SEGAN/model_weights_saved', type=str, help='model weights save path')
     parser.add_argument('--qc_plot', default=False, type=bool, help='plot qc')
 
-    opt = parser.parse_args()
-    BATCH_SIZE = opt.batch_size
-    NUM_EPOCHS = opt.num_epochs
-    TEST_CLEAN_DATA_PATH = opt.test_clean_data_path
-    TEST_NOISY_DATA_PATH = opt.test_noisy_data_path
-    TEST_DATA_PATH = opt.test_data_path
-    OUTPUT_DATA_PATH = opt.output_data_path
-    MODEL_SAVE_PATH = opt.model_save_path
-    MODEL_WEIGHTS_SAVE_PATH = opt.model_weights_save_path
-    QC_PLOT = opt.qc_plot
+    return parser.parse_args()
 
-    # --------------------------------------------------
-    # 2.0 Load dataset for testing
-    # --------------------------------------------------
+def load_datasets(opt):
     print('loading the datasets for testing...')
+    test_CleanFile_lists = sorted(glob.glob(os.path.join(opt.test_clean_data_path, '*')))
+    test_NoisyFile_lists = sorted(glob.glob(os.path.join(opt.test_noisy_data_path, '*')))
+    return test_CleanFile_lists, test_NoisyFile_lists
 
-    # generate test file list
-    test_CleanFile_lists = sorted(glob.glob(os.path.join(TEST_CLEAN_DATA_PATH, '*')))
-    test_NoisyFile_lists = sorted(glob.glob(os.path.join(TEST_NOISY_DATA_PATH, '*')))
-
-    # --------------------------------------------------
-    # 3.0 Define the model
-    # --------------------------------------------------
+def define_and_load_model(opt, num_epochs):
     print('Defining the generator model...')
     generator = GeneratorSegan()
 
     print('get the latest weights...')
-    for epoch in range(NUM_EPOCHS, 0, -1):
-        if os.path.exists(f'{MODEL_WEIGHTS_SAVE_PATH}/generator_weights_epoch_{epoch}.index'):
-            generator.load_weights(f'{MODEL_WEIGHTS_SAVE_PATH}/generator_weights_epoch_{epoch}')
+    for epoch in range(num_epochs, 0, -1):
+        if os.path.exists(f'{opt.model_weights_save_path}/generator_weights_epoch_{epoch}.index'):
+            generator.load_weights(f'{opt.model_weights_save_path}/generator_weights_epoch_{epoch}')
             print('load weights from epoch %d' % epoch)
             break
-    # --------------------------------------------------
-    # 4.0 Test the denoising
-    # --------------------------------------------------
-    print('denoising based on the generator model...')
+    return generator
 
-    # set the parameters
+def denoise_data(opt, generator, test_CleanFile_lists, test_NoisyFile_lists):
+    print('denoising based on the generator model...')
     window_size = 2 ** 14  # about 1 second of samples
     sample_rate = 16000
     stride      = 0.5
 
     for noisy_sound_file,clean_sound_file in tqdm(zip(test_NoisyFile_lists,test_CleanFile_lists)):
-
         # check if the denoising has been applied
-        if os.path.exists(f'{OUTPUT_DATA_PATH}/denoised_' + noisy_sound_file.split('/')[-1]):
+        if os.path.exists(f'{opt.output_data_path}/denoised_' + noisy_sound_file.split('/')[-1]):
             print('denoised_' + noisy_sound_file.split('/')[-1] + 'has been generated! move to next file...')
-
         else:
             # load the clean data
             noisy_sound, _ = librosa.load(noisy_sound_file, sr=sample_rate)
@@ -116,16 +94,6 @@ if __name__ == '__main__':
                 clean_slice    = tf.transpose(np.array(clean_slice),[0, 2, 1])
                 noisy_slice    = tf.transpose(np.array(noisy_slice),[0, 2, 1])
 
-                # # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                # # beaware of the old implemenation has a bug with the emphasis and de-emphasis which are not reversible, thus the preempasis is not removed after the deemphasis. the amplitude of the output is much weaker than the original input!!!!
-                # # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                # denoised_slice = emphasis(denoised_slice, emph_coeff=0.95,pre=False)
-                # clean_slice    = emphasis(clean_slice, emph_coeff=0.95,pre=False)
-                # noisy_slice    = emphasis(noisy_slice, emph_coeff=0.95,pre=False)
-
-                # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                # use the following code to remove the emphasis after the deemphasis, implemenation from librosa
-                # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 denoised_slice = emphasis_librosa(denoised_slice.numpy(), emph_coeff=0.95, pre=False)
                 clean_slice    = emphasis_librosa(clean_slice.numpy(), emph_coeff=0.95, pre=False)
                 noisy_slice    = emphasis_librosa(noisy_slice.numpy(), emph_coeff=0.95, pre=False)
@@ -136,7 +104,7 @@ if __name__ == '__main__':
                 clean_slices_qc.append(clean_slice)
 
                 # QC on a single data batch
-                if QC_PLOT:
+                if opt.qc_plot:
                     plt.figure()
                     plt.plot(np.reshape(noisy_slice,-1), label='noisy')
                     plt.plot(np.reshape(clean_slice,-1), label='clean')
@@ -152,9 +120,7 @@ if __name__ == '__main__':
             clean_sound_qc     = Waveform_generator.reconstruct_from_slice(clean_slices_qc, window_size=window_size, stride=stride)
             noisy_sound_qc     = Waveform_generator.reconstruct_from_slice(noisy_slices_qc, window_size=window_size, stride=stride)
 
-
-            if QC_PLOT:
-
+            if opt.qc_plot:
                 # QC on a single data batch
                 plt.figure()
                 plt.plot(noisy_sound_qc, label='noisy')
@@ -166,7 +132,16 @@ if __name__ == '__main__':
                 plt.show()
 
             # writout the denoised data
-            print('writing denoised data to {}'.format(OUTPUT_DATA_PATH))
-            sf.write(f'{OUTPUT_DATA_PATH}/denoised_' + noisy_sound_file.split('/')[-1], denoised_sound, sample_rate)
-            sf.write(f'{OUTPUT_DATA_PATH}/clean_' + noisy_sound_file.split('/')[-1], clean_sound_qc, sample_rate)
-            sf.write(f'{OUTPUT_DATA_PATH}/noisy_' + noisy_sound_file.split('/')[-1], noisy_sound_qc, sample_rate)
+            print('writing denoised data to {}'.format(opt.output_data_path))
+            sf.write(f'{opt.output_data_path}/denoised_' + noisy_sound_file.split('/')[-1], denoised_sound, sample_rate)
+            sf.write(f'{opt.output_data_path}/clean_' + noisy_sound_file.split('/')[-1], clean_sound_qc, sample_rate)
+            sf.write(f'{opt.output_data_path}/noisy_' + noisy_sound_file.split('/')[-1], noisy_sound_qc, sample_rate)
+
+def main():
+    opt = parse_arguments()
+    test_CleanFile_lists, test_NoisyFile_lists = load_datasets(opt)
+    generator = define_and_load_model(opt, opt.num_epochs)
+    denoise_data(opt, generator, test_CleanFile_lists, test_NoisyFile_lists)
+
+if __name__ == '__main__':
+    main()
